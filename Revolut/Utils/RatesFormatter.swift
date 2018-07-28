04b97714de52bf8f103f057b44874255236c693e
Anton Chebotov <anton.chebotov@aviasales.ru>
2018-07-28 15:44:45 +0300

    RatesFormatter + tests

--- Revolut/Utils/RatesFormatter.swift
+++ Revolut/Utils/RatesFormatter.swift
@@ -4,6 +4,8 @@ class RatesFormatter {
 
     private static let formatter: NumberFormatter = {
         let formatter = NumberFormatter()
+        formatter.roundingMode = .floor
+        formatter.numberStyle = .decimal
         formatter.maximumFractionDigits = 2
         formatter.minimumFractionDigits = 2
         formatter.minimumIntegerDigits = 1
@@ -12,6 +14,9 @@ class RatesFormatter {
     }()
 
     static func displayText(_ value: Double) -> String? {
+        if value.truncatingRemainder(dividingBy: 1) == 0 {
+            return String(format: "%.0f", value)
+        }
         return formatter.string(from: NSNumber(value: value))
     }
 }
--- RevolutTests/RatesConverterTests.swift
+++ RevolutTests/RatesConverterTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+@testable import Revolut
 
 class RatesConverterTests: XCTestCase {
     
